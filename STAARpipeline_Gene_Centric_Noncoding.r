745d12e3107cb4136cd77e90bc175e9b5902450d
Kevin Doyle <kwdoyle1@gmail.com>
2024-01-18 16:33:25 -0500

    updated gene centric noncoding analysis to match format of other steps too

--- STAARpipeline_Gene_Centric_Noncoding.r
+++ STAARpipeline_Gene_Centric_Noncoding.r
@@ -18,10 +18,13 @@ library(STAARpipeline)
 ###########################################################
 #           User Input
 ###########################################################
+chr <- as.numeric(commandArgs(TRUE)[1])
+basedir <- commandArgs(TRUE)[2]
+
 ## aGDS directory
-agds_dir <- get(load("/path_to_the_file/agds_dir.Rdata"))
+agds_dir <- get(load(paste0(basedir, "/AssociationAnalysisPrestep/agds_dir.Rdata")))
 ## Null model
-obj_nullmodel <- get(load("/path_to_the_file/obj_nullmodel.Rdata"))
+obj_nullmodel <- get(load(paste0(basedir, "/staar_null_model/obj_nullmodel.Rdata")))
 
 ## QC_label
 QC_label <- "annotation/filter"
@@ -33,7 +36,7 @@ geno_missing_imputation <- "mean"
 ## Annotation_dir
 Annotation_dir <- "annotation/info/FunctionalAnnotation"
 ## Annotation channel
-Annotation_name_catalog <- get(load("/path_to_the_file/Annotation_name_catalog.Rdata"))
+Annotation_name_catalog <- get(load(paste0(basedir, "/AssociationAnalysisPrestep/Annotation_name_catalog.Rdata")))
 # Or equivalently
 # Annotation_name_catalog <- read.csv("/path_to_the_file/Annotation_name_catalog.csv")
 ## Use_annotation_weights
@@ -42,64 +45,84 @@ Use_annotation_weights <- TRUE
 Annotation_name <- c("CADD","LINSIGHT","FATHMM.XF","aPC.EpigeneticActive","aPC.EpigeneticRepressed","aPC.EpigeneticTranscription",
                      "aPC.Conservation","aPC.LocalDiversity","aPC.Mappability","aPC.TF","aPC.Protein")
 
+# Masks to exclude
+#mask_file <- "/home/STAAR/potential_masks/potential_noncoding_masks_chr_5_11_20_any_pval_eq_1.csv"
+
 ## output path
-output_path <- "/path_to_the_output_file/"
+output_path <- paste0(basedir, "/Gene_Centric_Noncoding_Analysis/")
+print(paste("Will create directory:", output_path))
+dir.create(output_path)
 ## output file name
 output_file_name <- "TOPMed_F5_LDL_Noncoding"
 ## input array id from batch file (Harvard FAS RC cluster)
-arrayid <- as.numeric(commandArgs(TRUE)[1])
+#arrayid <- as.numeric(commandArgs(TRUE)[1])
+# use chromosome as only input
 
 ###########################################################
 #           Main Function 
 ###########################################################
 ## gene number in job
-gene_num_in_array <- 50 
-group.num.allchr <- ceiling(table(genes_info[,2])/gene_num_in_array)
-sum(group.num.allchr)
-
-chr <- which.max(arrayid <= cumsum(group.num.allchr))
-group.num <- group.num.allchr[chr]
-
-if (chr == 1){
-  groupid <- arrayid
-}else{
-  groupid <- arrayid - cumsum(group.num.allchr)[chr-1]
-}
+#gene_num_in_array <- 50 
+#group.num.allchr <- ceiling(table(genes_info[,2])/gene_num_in_array)
+#sum(group.num.allchr)
+#
+#chr <- which.max(arrayid <= cumsum(group.num.allchr))
+#group.num <- group.num.allchr[chr]
+#
+#if (chr == 1){
+#  groupid <- arrayid
+#}else{
+#  groupid <- arrayid - cumsum(group.num.allchr)[chr-1]
+#}
 
 genes_info_chr <- genes_info[genes_info[,2]==chr,]
 sub_seq_num <- dim(genes_info_chr)[1]
 
-if(groupid < group.num)
-{
-  sub_seq_id <- ((groupid - 1)*gene_num_in_array + 1):(groupid*gene_num_in_array)
-}else
-{
-  sub_seq_id <- ((groupid - 1)*gene_num_in_array + 1):sub_seq_num
-}
+#if(groupid < group.num)
+#{
+#  sub_seq_id <- ((groupid - 1)*gene_num_in_array + 1):(groupid*gene_num_in_array)
+#}else
+#{
+#  sub_seq_id <- ((groupid - 1)*gene_num_in_array + 1):sub_seq_num
+#}
+#
+### exclude large noncoding masks
+#jobid_exclude <- c(21,39,44,45,46,53,55,83,88,103,114,127,135,150,154,155,163,164,166,180,189,195,200,233,280,285,295,313,318,319,324,327,363,44,45,54)
+#sub_seq_id_exclude <- c(1009,1929,182,214,270,626,741,894,83,51,611,385,771,493,671,702,238,297,388,352,13,303,600,170,554,207,724,755,1048,319,324,44,411,195,236,677)
+#
+#for(i in 1:length(jobid_exclude))
+#{
+#  if(arrayid==jobid_exclude[i])
+#  {
+#    sub_seq_id <- setdiff(sub_seq_id,sub_seq_id_exclude[i])
+#  }
+#}
 
-## exclude large noncoding masks
-jobid_exclude <- c(21,39,44,45,46,53,55,83,88,103,114,127,135,150,154,155,163,164,166,180,189,195,200,233,280,285,295,313,318,319,324,327,363,44,45,54)
-sub_seq_id_exclude <- c(1009,1929,182,214,270,626,741,894,83,51,611,385,771,493,671,702,238,297,388,352,13,303,600,170,554,207,724,755,1048,319,324,44,411,195,236,677)
-
-for(i in 1:length(jobid_exclude))
-{
-  if(arrayid==jobid_exclude[i])
-  {
-    sub_seq_id <- setdiff(sub_seq_id,sub_seq_id_exclude[i])
-  }
-}
+# new way to exclude masks--by explicit gene name within a file
+# don't do this
+#mask_df <- read.csv(mask_file)
+## remove masks from gene info table.
+## this mask df has mask genes from all chrs, but should still be able to use it here like this.
+#print("Removing mask genes")
+#genes_info_chr = genes_info_chr[which(!genes_info_chr$hgnc_symbol %in% mask_df$hgnc_symbol), ]
+## and re-set the row index value to loop over..
+#sub_seq_num <- dim(genes_info_chr)[1]
 
 ## aGDS file
 agds.path <- agds_dir[chr]
 genofile <- seqOpen(agds.path)
 
-genes <- genes_info
+# still not used in this script either..
+#genes <- genes_info
 
 results_noncoding <- c()
-for(kk in sub_seq_id)
+#for(kk in sub_seq_id)
+# loop over each gene in the current chromosome
+for(kk in 1:sub_seq_num)
 {
-  print(kk)
+  #print(kk)
   gene_name <- genes_info_chr[kk,1]
+  print(gene_name)
   results <- Gene_Centric_Noncoding(chr=chr,gene_name=gene_name,genofile=genofile,obj_nullmodel=obj_nullmodel,
                                     rare_maf_cutoff=0.01,rv_num_cutoff=2,
                                     QC_label=QC_label,variant_type=variant_type,geno_missing_imputation=geno_missing_imputation,
@@ -109,7 +132,7 @@ for(kk in sub_seq_id)
   results_noncoding <- append(results_noncoding,results)
 }
 
-save(results_noncoding,file=paste0(output_path,output_file_name,"_",arrayid,".Rdata"))
+save(results_noncoding,file=paste0(output_path,output_file_name,"_",chr,".Rdata"))
 
 seqClose(genofile)
 
