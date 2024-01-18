d10ff746365c76a7f1ec5a3ec361ec86e571bb0d
Kevin Doyle <kwdoyle1@gmail.com>
2024-01-18 16:35:31 -0500

    remove parallel processing, as this does not work when submitted as a job to the cluster

--- convertVCF2GDS.R
+++ convertVCF2GDS.R
@@ -4,6 +4,7 @@
 # VCF Input: can be one VCF or several, as a character vector
 # Author(s): Michael R. Brown, Jennifer A. Brody
 
+
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 import.format <- args[1] # characters, the variable name(s) in the FORMAT field for import; or NULL for all variables
@@ -16,14 +17,16 @@ print(vcf.file)
 # GDS Ouput
 gds.file <- paste0(base.filename,".gds")
 
+
 library(SeqArray)
 
 # Conversion -- This could take some time (if the input files are large)
-nSlots <- system("nproc",intern=T)
-nThreads <- ifelse(is.na(strtoi(nSlots) >= 1), 1, strtoi(nSlots))
-if (nThreads == 0) nThreads <- 1
-message(paste("Running with", nThreads,"thread(s)."))
-Sys.setenv(MKL_NUM_THREADS=nThreads)
+# note: on the sge cluster, using the parallel argument causes the job to instantly crash.
+#nSlots <- system("nproc",intern=T)
+#nThreads <- ifelse(is.na(strtoi(nSlots) >= 1), 1, strtoi(nSlots))
+#if (nThreads == 0) nThreads <- 1
+#message(paste("Running with", nThreads,"thread(s)."))
+#Sys.setenv(MKL_NUM_THREADS=nThreads)
 
 
 if(fileformat == 'bcf'){
@@ -34,7 +37,7 @@ if(fileformat == 'bcf'){
 	seqBCF2GDS(vcf.file,gds.file,fmt.import=import.format, storage.option="LZMA_RA",bcftools="bcftools")
 }else{
     message("converting VCF")
-	seqVCF2GDS(vcf.file,gds.file,fmt.import=import.format, storage.option="LZMA_RA",parallel=nThreads)
+	seqVCF2GDS(vcf.file,gds.file,fmt.import=import.format, storage.option="LZMA_RA")  #,parallel=nThreads)
 }
 # Quick summary for log's sake
 g <- seqOpen(gds.file)
